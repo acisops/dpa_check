0430ceadfce02aa22b8dd1ee7a39f43bfde2dc10
John ZuHone <jzuhone@gmail.com>
2017-01-18 15:40:09 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+from dpa_check import __version__
+
+entry_points = {'console_scripts': 'dpa_check = dpa_check.dpa_check:main'}
+
+url = 'https://github.com/acisops/dpa_check/tarball/{}'.format(__version__)
+
+setup(name='dpa_check',
+      packages=["dpa_check"],
+      version=__version__,
+      description='ACIS Thermal Model for 1DPAMZT',
+      author='John ZuHone',
+      author_email='jzuhone@gmail.com',
+      url='http://github.com/acisops/dpa_check',
+      download_url=url,
+      include_package_data=True,
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+      ],
+      entry_points=entry_points,
+      )
