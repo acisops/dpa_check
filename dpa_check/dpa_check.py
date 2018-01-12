192a63c0451b917d7a0543ebc241bc7402a5db4b
John ZuHone <jzuhone@gmail.com>
2018-01-12 15:11:09 -0500

    Adopt the simpler signature

--- dpa_check/dpa_check.py
+++ dpa_check/dpa_check.py
@@ -23,34 +23,12 @@ import sys
 from acis_thermal_check import \
     ACISThermalCheck, \
     calc_off_nom_rolls, \
-    get_options, \
-    make_state_builder, \
-    get_acis_limits
+    get_options
 import os
 
 model_path = os.path.abspath(os.path.dirname(__file__))
 
-yellow_hi, red_hi = get_acis_limits("1dpamzt")
-
 MSID = {"dpa": '1DPAMZT'}
-# This is the Yellow High IPCL limit.
-# 05/2014 - changed from 35.0 to 37.5
-YELLOW = {"dpa": yellow_hi}
-# This is the difference between the Yellow High IPCL limit and 
-# the Planning Limit. So the Planning Limit is YELLOW - MARGIN
-#
-# 12/5/13 - This value was changed from 2.5 to 2.0 to reflect the new 
-# 1DPAMZT planning limit of 33 degrees C
-# 05/19/14 this is changed from 2.0, to 3.0.  2 degress for the normal
-#          padding for model error and an additional degree because
-#          the total change is being done in increments. We will back
-#          this off from 3 degrees to two after a few months trial 
-#          testing.  So for now the planning limit will be 34.5 deg. C.
-# 09/19/14 - Set MARGIN to 2.0 so that the Planning Limit is now 
-#            35.5 deg. C
-MARGIN = {"dpa": 2.0}
-# 12/5/13 - Likewise the 1DPAMZT validation limits were reduced to 2.0 
-#           from 2.5 for the 1% and 99% quantiles
 VALIDATION_LIMITS = {'1DPAMZT': [(1, 2.0), (50, 1.0), (99, 2.0)],
                      'PITCH': [(1, 3.0), (99, 3.0)],
                      'TSCPOS': [(1, 2.5), (99, 2.5)]
@@ -75,12 +53,11 @@ def calc_model(model_spec, states, start, stop, T_dpa=None, T_dpa_times=None):
 
 def main():
     args = get_options("dpa", model_path)
-    state_builder = make_state_builder(args.state_builder, args)
-    dpa_check = ACISThermalCheck("1dpamzt", "dpa", MSID, YELLOW,
-                                 MARGIN, VALIDATION_LIMITS,
-                                 HIST_LIMIT, calc_model)
+    dpa_check = ACISThermalCheck("1dpamzt", "dpa", MSID,
+                                 VALIDATION_LIMITS, HIST_LIMIT,
+                                 calc_model, args)
     try:
-        dpa_check.driver(args, state_builder)
+        dpa_check.driver()
     except Exception as msg:
         if args.traceback:
             raise
