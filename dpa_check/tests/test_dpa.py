ff71d221e0b287a46fc8b6bc879154f335f4385d
John ZuHone <jzuhone@gmail.com>
2018-02-21 21:35:10 -0500

    Try to run these tests like this

--- dpa_check/tests/test_dpa.py
+++ dpa_check/tests/test_dpa.py
@@ -1,11 +1,38 @@
 from ..dpa_check import VALIDATION_LIMITS, \
     HIST_LIMIT, calc_model, model_path
 from acis_thermal_check.regression_testing import \
-    RegressionTester
+    RegressionTester, test_loads
+import pytest
 
 dpa_rt = RegressionTester("1dpamzt", "dpa", model_path, VALIDATION_LIMITS,
                           HIST_LIMIT, calc_model)
 
-def test_dpa_loads(answer_store, **kwargs):
-    dpa_rt.run_test_arrays(answer_store, **kwargs)
 
+@pytest.mark.parametrize('load', test_loads["normal"])
+def test_normal_results(answer_store, load):
+    dpa_rt.run_answer_test(answer_store, load)
+
+
+@pytest.mark.parametrize('load', test_loads["normal"])
+def test_normal_images(answer_store, load):
+    dpa_rt.run_image_test(answer_store, load)
+
+
+@pytest.mark.parametrize('load', test_loads["too"])
+def test_too_results(answer_store, load):
+    dpa_rt.run_answer_test(answer_store, load)
+
+
+@pytest.mark.parametrize('load', test_loads["too"])
+def test_too_images(answer_store, load):
+    dpa_rt.run_image_test(answer_store, load)
+
+
+@pytest.mark.parametrize('load', test_loads["stop"])
+def test_stop_results(answer_store, load):
+    dpa_rt.run_answer_test(answer_store, load)
+
+
+@pytest.mark.parametrize('load', test_loads["stop"])
+def test_stop_images(answer_store, load):
+    dpa_rt.run_image_test(answer_store, load)
