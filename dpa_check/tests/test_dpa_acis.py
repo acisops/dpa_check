c5f9e09f883a41b6ce2ecbb33409297951350440
John ZuHone <jzuhone@gmail.com>
2018-09-07 11:03:03 -0400

    This has to be split up

--- dpa_check/tests/test_dpa_acis.py
+++ dpa_check/tests/test_dpa_acis.py
@@ -22,20 +22,3 @@ def test_prediction_acis(answer_store, load):
 @pytest.mark.parametrize('load', all_loads)
 def test_validation_acis(answer_store, load):
     dpa_rt.run_test("validation", answer_store, load)
-
-# SQL state builder tests
-
-dpa_rt.run_models(state_builder='sql')
-
-# Prediction tests
-
-@pytest.mark.parametrize('load', all_loads)
-def test_prediction_sql(answer_store, load):
-    dpa_rt.run_test("prediction", answer_store, load)
-
-# Validation tests
-
-@pytest.mark.parametrize('load', all_loads)
-def test_validation_sql(answer_store, load):
-    dpa_rt.run_test("validation", answer_store, load)
-
---
+++ dpa_check/tests/test_dpa_sql.py
@@ -0,0 +1,25 @@
+from ..dpa_check import VALIDATION_LIMITS, \
+    HIST_LIMIT, calc_model, model_path
+from acis_thermal_check.regression_testing import \
+    RegressionTester, all_loads
+import pytest
+
+dpa_rt = RegressionTester("1dpamzt", "dpa", model_path, VALIDATION_LIMITS,
+                          HIST_LIMIT, calc_model)
+
+# SQL state builder tests
+
+dpa_rt.run_models(state_builder='sql')
+
+# Prediction tests
+
+@pytest.mark.parametrize('load', all_loads)
+def test_prediction_acis(answer_store, load):
+    dpa_rt.run_test("prediction", answer_store, load)
+
+# Validation tests
+
+@pytest.mark.parametrize('load', all_loads)
+def test_validation_acis(answer_store, load):
+    dpa_rt.run_test("validation", answer_store, load)
+
