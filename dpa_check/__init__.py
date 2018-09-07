66fb43a45bacaf658a33061bf51a0152bb462165
John ZuHone <jzuhone@gmail.com>
2018-09-07 10:52:20 -0400

    add a hook for the tests here

--- dpa_check/__init__.py
+++ dpa_check/__init__.py
@@ -2,3 +2,11 @@ __version__ = "2.3.0"
 
 from .dpa_check import \
     calc_model
+
+
+def test(*args, **kwargs):
+    """
+    Run py.test unit tests.
+    """
+    import testr
+    return testr.test(*args, **kwargs)
