55848aec8741e8bfc498dde23b6ab05ff5a08fc9
John ZuHone <jzuhone@gmail.com>
2018-09-11 17:06:42 -0400

    Test violation reporting for 1DPAMZT

---
+++ dpa_check/tests/test_dpa_viols.py
@@ -0,0 +1,13 @@
+from ..dpa_check import VALIDATION_LIMITS, \
+    HIST_LIMIT, calc_model, model_path
+from acis_thermal_check.regression_testing import \
+    RegressionTester
+import os
+
+dpa_rt = RegressionTester("1dpamzt", "dpa", model_path, VALIDATION_LIMITS,
+                          HIST_LIMIT, calc_model)
+
+def test_JUL3018_viols():
+    model_spec = os.path.join(os.path.dirname(__file__),
+                              "JUL3018_model_spec.json")
+    dpa_rt.check_violation_reporting("JUL3018", model_spec)
