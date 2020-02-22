ce99aeb916ef35159bdcc2aa6112327bfe344fd1
John ZuHone <jzuhone@gmail.com>
2020-02-21 23:38:38 -0500

    Get the answer_store argument to work here

--- dpa_check/__init__.py
+++ dpa_check/__init__.py
@@ -12,5 +12,8 @@ def test(*args, **kwargs):
     Run py.test unit tests.
     """
     import testr
+    if kwargs.get("answer_store", False) and '--answer_store' not in args:
+        args += ("--answer_store",)
+        kwargs.pop("answer_store")
     return testr.test(*args, **kwargs)
 
